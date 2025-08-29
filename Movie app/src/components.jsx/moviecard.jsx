bb126cf6f45062deb5cef529f378b3927e63e40c
Mickey-Oduro-Kwarteng <odurokwartengmickey@gmail.com>
2025-08-30 10:47:31 +1100

    Initial commit

---
+++ Movie app/src/components.jsx/moviecard.jsx
@@ -0,0 +1,24 @@
+function MovieCard({ movie }) {
+  function onfavouriteclick() {
+    alert("clicked");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-button">
+          <button className="favorite-btn" onClick={onfavouriteclick}>
+            ❤️
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>
+            {movie.title}
+        </h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+}
